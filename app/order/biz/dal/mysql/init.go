504532f93845cb5fd22799d4e8e449f999b88487
Qiang <2844326483@qq.com>
2024-12-08 10:25:16 +0800

    docker部署

--- app/order/biz/dal/mysql/init.go
+++ app/order/biz/dal/mysql/init.go
@@ -17,7 +17,7 @@ var (
 )
 
 func Init() {
-	dsn := fmt.Sprintf(conf.GetConf().MySQL.DSN, os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_HOST"))
+	dsn := fmt.Sprintf(conf.GetConf().MySQL.DSN, os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"))
 	//dsn := fmt.Sprintf("%s:%s@tcp(%s:3307)/user?charset=utf8mb4&parseTime=True&loc=Local", os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_HOST"))
 	DB, err = gorm.Open(mysql.Open(dsn),
 		&gorm.Config{
--- app/user/biz/dal/mysql/init.go
+++ app/user/biz/dal/mysql/init.go
@@ -2,6 +2,7 @@ package mysql
 
 import (
 	"biz-demo/gomall/app/user/biz/model"
+	"biz-demo/gomall/app/user/conf"
 	"fmt"
 	"gorm.io/plugin/opentelemetry/tracing"
 	"os"
@@ -16,8 +17,8 @@ var (
 )
 
 func Init() {
-	//dsn := fmt.Sprintf(conf.GetConf().MySQL.DSN, os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_HOST"))
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:3307)/user?charset=utf8mb4&parseTime=True&loc=Local", os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_HOST"))
+	dsn := fmt.Sprintf(conf.GetConf().MySQL.DSN, os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"))
+	//dsn := fmt.Sprintf("%s:%s@tcp(%s:3307)/user?charset=utf8mb4&parseTime=True&loc=Local", os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_HOST"))
 	DB, err = gorm.Open(mysql.Open(dsn),
 		&gorm.Config{
 			PrepareStmt:            true,
